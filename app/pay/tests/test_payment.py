8ae9e33c6eb36ec4407ef40184a88768de33d3f1
PilarczykM <marcinpilarczyk@me.com>
2022-06-21 15:59:33 +0200

    Update test_payment.py
    Using MonkeyPatch mock user input

--- app/pay/tests/test_payment.py
+++ app/pay/tests/test_payment.py
@@ -1,9 +1,20 @@
 import pytest
-from pay.order import Order
+
+from pay.order import Order, LineItem
 from pay.payment import pay_order
 
+from pytest import MonkeyPatch
 
 def test_pay_order_with_total_zero_raises_value_error() -> None:
     order = Order()
     with pytest.raises(ValueError):
         pay_order(order)
+
+
+def test_pay_order_default(monkeypatch: MonkeyPatch) -> None:
+    responses = iter(["1249190007575069", "12", "2024"])
+    monkeypatch.setattr("builtins.input", lambda _: next(responses))
+
+    order = Order()
+    order.line_items.append(LineItem(name="Test", price=100))
+    pay_order(order)
