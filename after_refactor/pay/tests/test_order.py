2f45678a84cef2b9a2c93d2070ad0ae5ae02bc4b
PilarczykM <marcinpilarczyk@me.com>
2022-06-21 16:34:19 +0200

    copy app

---
+++ after_refactor/pay/tests/test_order.py
@@ -0,0 +1,19 @@
+from pay.order import Order, OrderStatus, LineItem
+
+
+def test_order_default() -> None:
+    order = Order()
+    assert order.status == OrderStatus.OPEN
+    assert order.total == 0
+
+
+def test_order_with_one_item() -> None:
+    order = Order()
+    order.line_items.append(LineItem(name="test", price=100))
+    assert order.total == 100
+
+
+def test_order_pay_change_status() -> None:
+    order = Order()
+    order.pay()
+    order.status == OrderStatus.PAID
