bd99a304bdeed72dfff5952ad6b92d1cb5c3aafd
PilarczykM <marcinpilarczyk@me.com>
2022-06-21 15:41:53 +0200

    Update test_payment.py
    Rename

--- app/pay/tests/test_payment.py
+++ app/pay/tests/test_payment.py
@@ -3,7 +3,7 @@ from pay.order import Order
 from pay.payment import pay_order
 
 
-def test_pay_order_with_total_zero_rises_value_error() -> None:
+def test_pay_order_with_total_zero_raises_value_error() -> None:
     order = Order()
     with pytest.raises(ValueError):
         pay_order(order)
