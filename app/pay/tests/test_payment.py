e3efdee198fb1a5c67f717bb6da676da1338230a
PilarczykM <marcinpilarczyk@me.com>
2022-06-21 16:07:17 +0200

    Update test_payment.py
    Mock check_api_key and charge function

--- app/pay/tests/test_payment.py
+++ app/pay/tests/test_payment.py
@@ -5,6 +5,9 @@ from pay.payment import pay_order
 
 from pytest import MonkeyPatch
 
+from pay.processor import PaymentProcessor
+
+
 def test_pay_order_with_total_zero_raises_value_error() -> None:
     order = Order()
     with pytest.raises(ValueError):
@@ -12,8 +15,13 @@ def test_pay_order_with_total_zero_raises_value_error() -> None:
 
 
 def test_pay_order_default(monkeypatch: MonkeyPatch) -> None:
+    def charge_mock(self: PaymentProcessor, card: str, month: int, year: int, amount: int) -> None:
+        pass
+
     responses = iter(["1249190007575069", "12", "2024"])
     monkeypatch.setattr("builtins.input", lambda _: next(responses))
+    monkeypatch.setattr(PaymentProcessor, "_check_api_key", lambda _: True)
+    monkeypatch.setattr(PaymentProcessor, "charge", charge_mock)
 
     order = Order()
     order.line_items.append(LineItem(name="Test", price=100))
