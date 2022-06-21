ef747d91ef10e1edfe05fdbb5d71f23ef4e0d7e3
PilarczykM <marcinpilarczyk@me.com>
2022-06-21 17:23:15 +0200

    Use credit card object

--- after_refactor/pay/payment.py
+++ after_refactor/pay/payment.py
@@ -1,13 +1,15 @@
 from typing import Protocol
+
+from pay.credit_card import CreditCard
 from pay.order import Order
 
 
 class Payment(Protocol):
-    def charge(self, card: str, month: int, year: int, amount: int) -> None:
+    def charge(self, card: CreditCard, amount: int) -> None:
         """Charges the card with the amount"""
 
 
-def pay_order(order: Order, payment: Payment):
+def pay_order(order: Order, card: CreditCard, payment: Payment):
     if order.total == 0:
         raise ValueError("Can't pay an order with total 0.")
     card = input("Please enter card number: ")
