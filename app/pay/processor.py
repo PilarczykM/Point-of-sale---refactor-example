024d17d480d180191a8c8f04bb882b27a737ec7a
PilarczykM <marcinpilarczyk@me.com>
2022-06-21 14:08:55 +0200

    Base flow
    Add base flow to main

--- app/pay/processor.py
+++ app/pay/processor.py
@@ -5,9 +5,15 @@ class PaymentProcessor:
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
 
+    def _check_api_key(self) -> bool:
+        return self.api_key == "6cfb67f3-6281-4031-b893-ea85db0dce20"
+
     def charge(self, card: str, month: int, year: int, amount: int) -> None:
         if not self.validate_card(card, month, year):
             raise ValueError('Invalid Card')
+        if not self._check_api_key():
+            raise ValueError("Invalid API key")
+        print(f"Charging card number {card} for ${amount / 100:.2f}")
 
     def validate_card(self, card, month, year) -> bool:
         return self.luhn_checksum(card) and datetime(year, month, 1) > datetime.now()
